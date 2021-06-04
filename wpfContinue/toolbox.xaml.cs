cf8634a1ca6b413ed921ccf619622c7f5005ab54
Denis <denis.denisenko@gmail.com>
2021-06-04 16:29:54 +0300

    Add project files.

---
+++ wpfContinue/toolbox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfContinue
+{
+    /// <summary>
+    /// Interaction logic for toolbox.xaml
+    /// </summary>
+    public partial class toolbox : Window
+    {
+        public toolbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
